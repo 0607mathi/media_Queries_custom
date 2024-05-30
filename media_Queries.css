e348b36339f16e53c84106974c565716dc797c79
0607mathi <mathiyarasu.m0607@gmail.com>
2024-05-30 16:56:17 +0530

    custome media queries

---
+++ media_Queries.css
@@ -0,0 +1,23 @@
+/* https://github.com/0607mathi/media_Queries_custom.git */
+
+/* media querry */
+  
+@media (min-width: 427px) and (max-width: 1441px) {
+    
+    /* css */
+
+  }
+
+  
+  @media (min-width: 377px) and (max-width: 426px) {
+    
+    /* css */
+
+    
+  }
+  
+  @media (min-width: 320px) and (max-width: 376px) {
+    
+    /* css */
+
+  }
